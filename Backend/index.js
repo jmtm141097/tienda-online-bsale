d95787ae59fc1ab41b34d0e7d9980fe54b40c535
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-09-01 20:24:48 -0500

    cargando los datos de la bd y filtrando por categorias

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv')
 const cron = require('node-cron')
-const { mantenerConexion } = require('./src/utils/conexionBD')
+// const { mantenerConexion } = require('./src/utils/conexionBD')
 const app = require('./app.js')
 
 dotenv.config()
 
-cron.schedule('*/4 * * * * *', () => {
-    console.log('Reviviendo conexion')
-    mantenerConexion()
-})
+// cron.schedule('*/1 * * * * *', () => {
+//     console.log('Reviviendo conexion')
+//     mantenerConexion()
+// })
 
 app.listen(process.env.PORT || 3000, async () => {
     console.log('Corriendo por el puerto ' + process.env.PORT || 3000)
--- Frontend/app.js
+++ Frontend/app.js
@@ -1,1 +1,70 @@
-// alert('hola')
+const API_URL_CATEGORIES = 'http://localhost:3001/v1/api/'
+const HTMLCategories = document.querySelector('#categorias')
+const HTMLProducts = document.querySelector('#products')
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(`${API_URL_CATEGORIES}/categories`)
+        .then((response) => response.json())
+        .then((categories) => {
+            armarCategorias(categories)
+        })
+
+    fetch(`${API_URL_CATEGORIES}/products`)
+        .then((response) => response.json())
+        .then((products) => {
+            armarProductos(products)
+        })
+})
+
+const filtrarPorCategoria = (id, name) => {
+    fetch(`${API_URL_CATEGORIES}/products/${id}`)
+        .then((response) => response.json())
+        .then((products) => {
+            selectCategory(id)
+            document.getElementById('title').innerText = `Productos de la categoria ${name}`
+            armarProductos(products)
+        })
+}
+
+const selectCategory = (id) => {
+    const elems = document.querySelectorAll('.active')
+    ;[].forEach.call(elems, function (el) {
+        el.classList.remove('active')
+    })
+    document.getElementById(`category${id}`).classList.add('active')
+}
+
+const armarCategorias = (categories) => {
+    const liCategories = categories.reduce(
+        (acc, category) =>
+            (acc = acc.concat(
+                `<li class='list-group-item list-group-item-action' onclick='filtrarPorCategoria(${
+                    category.id
+                }, "${category.name.toString()}")' id='category${category.id}'>${category.name}</li>`
+            )),
+        ''
+    )
+    HTMLCategories.innerHTML = `<ul class='list-group'>${liCategories}</ul>`
+}
+
+const armarProductos = (products) => {
+    const cardProduct = products.reduce(
+        (acc, product) =>
+            (acc = acc.concat(`
+        <div class="col">
+            <div class="card h-100">
+                <img src="${product.url_image}" class="card-img-top" alt="${product.name}" />
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                </div>
+            </div>
+        </div>
+        `)),
+        ''
+    )
+    HTMLProducts.innerHTML = `${cardProduct}`
+}
